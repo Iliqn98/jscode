99158bc53d349de06213731ba5f3dafba50dde68
Todor Markov <todorator@gmail.com>
2019-05-10 22:07:59 +0300

    napravih express server

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 function sayHello(name) {
     result = 'Hello  ' + name
     console.log(result);
     return result;
 }
-sayHello ('Iliqn');
 
 function givePage1() {
-    return "";
+    return "Dadoh stranica 1";
 }
 
 function givePage2() {
-    return "";
+    return "Dadoh stranica 2";
 }
+
+app.get('/', (req, res) => res.send(sayHello("Nqma ime")));
+app.get('/page1', (req, res) => res.send(givePage1()));
+app.get('/page2', (req, res) => res.send(givePage2()));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
